b8ef2e06648e0b317cce668189e2d9bf9974c218
rahuldahal <rdaahal@gmail.com>
2025-02-20 20:11:51 +0545

    feat: add gunicorn as WSGI server and update configuration for environment variables

--- redalyze/config.py
+++ redalyze/config.py
@@ -1,11 +1,12 @@
+import os
 import praw
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 
-config = dotenv_values("../.env")
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
 
 def get_reddit_connection():
   return praw.Reddit(
-    client_id=config["REDDIT_CLIENT_ID"],
-    client_secret=config["REDDIT_SECRET"],
-    user_agent=config["REDDIT_USER_AGENT"]
+    client_id=os.getenv("REDDIT_CLIENT_ID"),
+    client_secret=os.getenv("REDDIT_SECRET"),
+    user_agent=os.getenv("REDDIT_USER_AGENT")
   )
--- redalyze/main.py
+++ redalyze/main.py
@@ -1,3 +1,4 @@
+import os
 import dash
 import pandas as pd
 import transform_data
@@ -90,4 +91,5 @@ dash_app.layout = html.Div([
 ])
 
 if __name__ == "__main__":
-  app.run(debug=True, port=5000)
+  app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
